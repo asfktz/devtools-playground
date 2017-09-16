ca994596d76b70a7b7f19ccda5548219b1f69ded
asaf <asf.ktz@gmail.com>
2017-09-16 22:39:38 +0300

    ignore .esm-cache

--- watch.js
+++ watch.js
@@ -5,7 +5,10 @@ const debounce = require('lodash.debounce');
 module.exports = (dir, callback) => {
   const watcher = chokidar
     .watch(dir, {
-      ignored: path.join(dir, 'node_modules'),
+      ignored: [
+        path.join(dir, 'node_modules'),
+        path.join(dir, '.esm-cache')
+      ],
       ignoreInitial: true
     })
     .on('all', debounce(callback, 30));
