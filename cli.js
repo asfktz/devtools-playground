b7974f0be33e53ceaac69ea3236c634cab2a41ba
Asaf Katz <asf.ktz@gmail.com>
2017-09-22 21:16:17 +0300

    version flag

--- cli.js
+++ cli.js
@@ -2,8 +2,16 @@
 
 const electron = require('electron');
 const proc = require('child_process');
+const version = require('./version');
+
+const args = process.argv.slice(2);
+const { log } = console;
+
+if (['--version', '-v'].includes(args[0])) {
+  return log(version);
+}
 
 // spawn Electron
-proc.spawn(electron, [__dirname, ...process.argv.slice(2)], {
+proc.spawn(electron, [__dirname, ...args], {
   // stdio: 'inherit'
 });
