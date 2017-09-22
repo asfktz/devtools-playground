b7d6dbbd077b2e28df0647da6756f2edc6774cfd
Asaf Katz <asf.ktz@gmail.com>
2017-09-22 21:17:08 +0300

    refactor require

--- renderer.js
+++ renderer.js
@@ -1,11 +1,19 @@
 const { remote } = require('electron');
+
+// Get the settings exposed by the main process (see main.js).
 const { entry, esm } = remote.getGlobal('settings');
 
-if (!esm) return require(entry);
+// Get enhanced require.
+const getRequire = esm => {
+  // No enhancements are needed.
+  if (!esm) return require;
 
-const esmRequire = require('@std/esm')(module, {
-  cjs: true,
-  esm: 'js'
-});
+  // With support for ES modules.
+  return require('@std/esm')(module, {
+    cjs: true,
+    esm: 'js'
+  });
+};
 
-esmRequire(entry);
+// Call the entry with the enhanced require method.
+getRequire(esm)(entry);
